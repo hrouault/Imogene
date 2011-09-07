3e316d2743fe7ed08d055c66011ed8cb4d17d122
Hervé Rouault <rouault@lps.ens.fr>
2011-09-07 16:27:23 +0200

    commandlist introduction
     On branch master
     Changes to be committed:
       (use "git reset HEAD <file>..." to unstage)
    
    	new file:   src/commandlist.txt
    	modified:   src/help.cpp

--- src/help.cpp
+++ src/help.cpp
@@ -20,17 +20,41 @@
 
 using namespace std;
 
-//struct cmdname_help {
-//   char name[16];
-//   char help [80];
-//}
+struct cmdname_help {
+   char name[16];
+   char help [80];
+};
 
-//static struct cmdname_help common_cmds[] = {}
+static struct cmdname_help common_cmds[] = {
+   {"extract", "Extract an alignment from a coordinate file"},
+   {"distinfo", "Distance between PWMs"},
+   {"genmot", "Generating motifs de novo"}
+};
+
+#define ARRAY_SIZE(x) (sizeof(x)/sizeof(x[0]))
+
+static inline void mput_char(char c, unsigned int num)
+{
+	while(num--)
+		putchar(c);
+}
 
 void
 list_common_cmds_help(void)
 {
+   int i, longest = 0;
 
+	for (i = 0; i < ARRAY_SIZE(common_cmds); i++) {
+		if (longest < strlen(common_cmds[i].name))
+			longest = strlen(common_cmds[i].name);
+	}
+
+	puts("The most commonly used imogene commands are:");
+	for (i = 0; i < ARRAY_SIZE(common_cmds); i++) {
+		printf("   %s   ", common_cmds[i].name);
+		mput_char(' ', longest - strlen(common_cmds[i].name));
+		puts(common_cmds[i].help);
+	}
 }
 //void
 //list_common_cmds_help(void)
