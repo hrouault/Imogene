b9d9ee9a985ad94edb628b9c93d42149d59f4b51
Hervé Rouault <rouault@lps.ens.fr>
2011-09-09 14:54:43 +0200

    help now works
     On branch master
     Changes to be committed:
       (use "git reset HEAD <file>..." to unstage)
    
    	modified:   src/Makefile.am
    	modified:   src/help.cpp
    	modified:   src/imogene.cpp
    	modified:   src/imogene.hpp

--- src/help.cpp
+++ src/help.cpp
@@ -19,6 +19,7 @@
 #include <iostream>
 
 #include "config.h"
+#include "imogene.hpp"
 
 using namespace std;
 
@@ -58,75 +59,13 @@ list_common_cmds_help(void)
 		puts(common_cmds[i].help);
 	}
 }
-//void
-//list_common_cmds_help(void)
-//{
-//	int i, longest = 0;
-//
-//	for (i = 0; i < ARRAY_SIZE(common_cmds); i++) {
-//		if (longest < strlen(common_cmds[i].name))
-//			longest = strlen(common_cmds[i].name);
-//	}
-//
-//	puts("The most commonly used git commands are:");
-//	for (i = 0; i < ARRAY_SIZE(common_cmds); i++) {
-//		printf("   %s   ", common_cmds[i].name);
-//		mput_char(' ', longest - strlen(common_cmds[i].name));
-//		puts(common_cmds[i].help);
-//	}
-//}
-
 
 int
 cmd_help(int argc, char **argv)
 {
-   int nongit;
-	const char *alias;
-	enum help_format parsed_help_format;
-	load_command_list("git-", &main_cmds, &other_cmds);
-
-	argc = parse_options(argc, argv, prefix, builtin_help_options,
-			builtin_help_usage, 0);
-	parsed_help_format = help_format;
-
-	if (show_all) {
-		printf("usage: %s\n\n", git_usage_string);
-		list_commands("git commands", &main_cmds, &other_cmds);
-		printf("%s\n", git_more_info_string);
-		return 0;
-	}
-
-	if (!argv[0]) {
-		printf("usage: %s\n\n", git_usage_string);
-		list_common_cmds_help();
-		printf("\n%s\n", git_more_info_string);
-		return 0;
-	}
-
-	setup_git_directory_gently(&nongit);
-	git_config(git_help_config, NULL);
-
-	if (parsed_help_format != HELP_FORMAT_NONE)
-		help_format = parsed_help_format;
-
-	alias = alias_lookup(argv[0]);
-	if (alias && !is_git_command(argv[0])) {
-		printf("`git %s' is aliased to `%s'\n", argv[0], alias);
-		return 0;
-	}
-
-	switch (help_format) {
-	case HELP_FORMAT_NONE:
-	case HELP_FORMAT_MAN:
-		show_man_page(argv[0]);
-		break;
-	case HELP_FORMAT_INFO:
-		show_info_page(argv[0]);
-		break;
-	case HELP_FORMAT_WEB:
-		show_html_page(argv[0]);
-		break;
-	}
+   cout <<  "Usage : " << usage_string << endl;
+   list_common_cmds_help();
+   cout << "\n" << more_info_string << endl;
 
 	return 0;
 }
--- src/imogene.hpp
+++ src/imogene.hpp
@@ -1,3 +1,6 @@
 
 #define ARRAY_SIZE(x) (sizeof(x)/sizeof(x[0]))
 
+extern const char usage_string[];
+
+extern const char more_info_string[];
