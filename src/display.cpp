0abbd8e06ddaf1b8f056869f554c90b99e137bc9
Hervé Rouault <rouault@lps.ens.fr>
2011-09-10 15:37:38 +0200

    update after install trial
     On branch master
     Changes to be committed:
       (use "git reset HEAD <file>..." to unstage)
    
    	modified:   configure.ac
    	modified:   doc/imogene.texi
    	new file:   doc/install.texi
    	modified:   doc/test.texi
    	new file:   src/display.cpp

---
+++ src/display.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+
+using namespace std;
+
+#include "display_cmdline.h"
+#include "const.hpp"
+
+display_args_info display_args;
+
+
+/** 
+ * ===  FUNCTION  ======================================================================
+ *         Name:  cmd_display
+ *  Description:  Display results
+ * =====================================================================================
+ */
+int
+cmd_display(int argc, char **argv)
+{
+   return 1;
+
+}		/* -----  end of function extract  ----- */
