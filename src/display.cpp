685a9822cd77078804023ffc06ecd27b61cb68e1
Hervé Rouault <herve.rouault@pasteur.fr>
2011-11-17 11:23:36 +0100

    remove .py from weblogo-display.py

--- src/display.cpp
+++ src/display.cpp
@@ -45,7 +45,7 @@ dispweblogo(vmot & mots)
     for (ivmot ivm = mots.begin(); ivm != mots.end(); ivm++) {
         if (ivm->check) {
             stringstream ss;
-            ss << "weblogo-display.py ";
+            ss << "weblogo-display ";
             ss << "Motif";
             ss << index << " ";
             ss << concc << " ";
