29458b6c50da8b96e7e45849ba8fe3196ea7df57
Marc Santolini <marc.santolini@gmail.com>
2013-04-11 19:08:36 +0200

    examples

---
+++ examples/extract/extract-eutherian.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+imogene extract -s eutherian -i coords-eutherian.bed 
+mv align align-eutherian
---
+++ examples/scangen/scangen-droso.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+imogene scangen -s droso -m ../genmot/motifs-droso.txt -t 10 --progress
