cf1c7a5d8cae6f92aa56922153623223b509ba5c
Hervé Rouault <rouaulth@janelia.hhmi.org>
2013-04-30 18:53:52 -0400

    update doc page

--- addstyle.sh
+++ addstyle.sh
@@ -3,18 +3,14 @@
 for i in doc-origin/*.html
 do
    defname=`echo $i | sed "s/doc-origin\///"`
-sed -e '1 s;^;<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"\
-        "http://www.w3.org/TR/html4/loose.dtd">\
-;' \
-    -e '/Content-Type/ s;">;\; charset=UTF-8">;'\
-    -e '/Content-Style-Type/ s;^;   <link rel="stylesheet" href="css/screen.css" type="text/css" media="screen, projection">\
+sed -e '/Content-Style-Type/ s;^;   <link rel="stylesheet" href="css/screen.css" type="text/css" media="screen, projection">\
    <link rel="stylesheet" href="css/print.css" type="text/css" media="print"> \
    <!--[if lt IE 8]>\
    <link rel="stylesheet" href="css/ie.css" type="text/css" media="screen, projection">\
    <![endif]-->\
    <link rel="stylesheet" href="css/texinfo.css" type="text/css" media="screen, projection"> \
    ;'\
-    -e '/<body>/ s;$;\
+    -e '/<body/ s;$;\
     <div class="container">\
     <div class="span-24">;'\
     -e 's;<samp>;<code>;'\
